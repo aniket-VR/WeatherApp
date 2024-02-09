79861f3f116e65c1d9e93f5d88cc93b49266a097
Rohokale Aniket <aniketrohokale2002@gmail.com>
2024-02-09 21:55:12 +0530

    header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./app/Components/Header/Header.tsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
